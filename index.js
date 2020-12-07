4aec2469a95488876f35b60ec9a9ccb7e4b6f161
Hector Mejia <caquillo722@gmail.com>
2020-12-06 21:17:40 -0500

    added docker compose and fixed config issues with sequelize

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 'use strict';
 
+require('dotenv').config()
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
-const {Sequelize} = require('sequelize');
 const {nanoid} = require('nanoid');
 const {Url} = require('./models');
-require('dotenv').config()
-
-const sequelize = new Sequelize(process.env.DATABASE_URL);
 
 const port = process.env.PORT || 8080;
-
 const app = express();
+
 app.use(cors());
 app.use(morgan("short"));
 app.use(express.json());
-
-
 
 app.get('/', (req, res)=> res.send('hello world!'));
 app.post('/url', async (req, res) => {
